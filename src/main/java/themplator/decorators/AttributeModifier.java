e6e0d54819cbba4876efad8876a8a9c85f033b0a
Jawher Moussa <jawher.moussa@gmail.com>
2010-06-23 00:02:14 +0100

    Removed the dummy bind method impl from AttributeModifier

--- src/main/java/themplator/decorators/AttributeModifier.java
+++ src/main/java/themplator/decorators/AttributeModifier.java
@@ -6,13 +6,11 @@ import java.util.List;
 import javax.xml.stream.XMLEventFactory;
 import javax.xml.stream.events.XMLEvent;
 
-import themplator.AbstractBrick;
 import themplator.AbstractDecorator;
-import themplator.Brick;
 import themplator.Model;
 import themplator.models.SimpleModel;
 
-public class AttributeModifier extends AbstractDecorator {
+public class AttributeModifier<T> extends AbstractDecorator<T> {
 	private String attrName;
 	private Model<String> attrValue;
 
@@ -21,27 +19,19 @@ public class AttributeModifier extends AbstractDecorator {
 		this.attrName = attrName;
 		this.attrValue = new SimpleModel<String>(attrValue);
 	}
-	
+
 	public AttributeModifier(String attrName, Model<String> attrValue) {
 		super();
 		this.attrName = attrName;
 		this.attrValue = attrValue;
 	}
 
-	public void bind(AbstractBrick brick) {
-
-	}
-
 	@SuppressWarnings("restriction")
 	@Override
 	public List<? extends XMLEvent> postBrickStartElement(
 			XMLEventFactory eventFactory) {
-		return Arrays.asList(eventFactory.createAttribute(attrName, attrValue.get()));
-	}
-
-	public void bind(Brick brick) {
-		// TODO Auto-generated method stub
-		
+		return Arrays.asList(eventFactory.createAttribute(attrName, attrValue
+				.get()));
 	}
 
 }
