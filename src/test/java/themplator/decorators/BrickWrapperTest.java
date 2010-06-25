8bc11d18367f1b605d64e58747c5f17d16938e79
Jawher Moussa <jawher.moussa@gmail.com>
2010-06-26 00:31:12 +0100

    Tested adding multiple BrickWrappers to a brick

--- src/test/java/themplator/decorators/BrickWrapperTest.java
+++ src/test/java/themplator/decorators/BrickWrapperTest.java
@@ -16,12 +16,22 @@ public class BrickWrapperTest extends BaseBrickTest<Brick> {
 				"resources/brick-wrapper1.output.xml", b);
 	}
 	
+	
 	@Test
 	public void testOuterWithAttrs() {
 		Brick b = new Brick("b");
 		b.add(new BrickWrapper("div", map(pair("id", "wrapper"), pair("class", "container"))));
 		test("resources/brick-wrapper1.input.xml",
 				"resources/brick-wrapper2.output.xml", b);
+	}
+	
+	@Test
+	public void test2OutersWithAttrs() {
+		Brick b = new Brick("b");
+		b.add(new BrickWrapper("div", map(pair("id", "wrapper"), pair("class", "container"))));
+		b.add(new BrickWrapper("p", map(pair("attr", "val"))));
+		test("resources/brick-wrapper1.input.xml",
+				"resources/brick-wrapper5.output.xml", b);
 	}
 	
 	@Test
@@ -38,6 +48,24 @@ public class BrickWrapperTest extends BaseBrickTest<Brick> {
 		b.add(new BrickWrapper("div", map(pair("id", "wrapper"), pair("class", "container")), WrapMode.INNER));
 		test("resources/brick-wrapper1.input.xml",
 				"resources/brick-wrapper4.output.xml", b);
+	}
+	
+	@Test
+	public void test2InnersWithAttrs() {
+		Brick b = new Brick("b");
+		b.add(new BrickWrapper("div", map(pair("id", "wrapper2")), WrapMode.INNER));
+		b.add(new BrickWrapper("div", map(pair("id", "wrapper"), pair("class", "container")), WrapMode.INNER));
+		test("resources/brick-wrapper1.input.xml",
+				"resources/brick-wrapper6.output.xml", b);
+	}
+	
+	@Test
+	public void testInnerAndOuterWithAttrs() {
+		Brick b = new Brick("b");
+		b.add(new BrickWrapper("div", map(pair("id", "wrapper2"))));
+		b.add(new BrickWrapper("div", map(pair("id", "wrapper"), pair("class", "container")), WrapMode.INNER));
+		test("resources/brick-wrapper1.input.xml",
+				"resources/brick-wrapper7.output.xml", b);
 	}
 
 }
